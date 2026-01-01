16f47626a6895f8e6ca46256aab003ebd4b931f9
Chris <christopherthomas55@gmail.com>
2026-01-01 11:22:43 -0600

    Better org link following behavior imo

--- init.el
+++ init.el
@@ -146,8 +146,15 @@
   (global-set-key (kbd "C-c c") #'org-capture)
   ;; Lambda because I was experimenting with new buffers and what not
   (add-hook 'org-capture-after-finalize-hook (lambda () (org-capture-goto-last-stored)))
-
   ;; Some defined in org-roam too
+
+
+  ;; Only difference compared to default is we find-file, not in other window or fram
+  (setq org-link-frame-setup
+	'((vm . vm-visit-folder-other-frame)
+	 (vm-imap . vm-visit-imap-folder-other-frame)
+	 (gnus . org-gnus-no-new-news) (file . find-file)
+	 (wl . wl-other-frame)))
 
   ;; Set up habits
   (add-to-list 'org-modules 'org-habit)
